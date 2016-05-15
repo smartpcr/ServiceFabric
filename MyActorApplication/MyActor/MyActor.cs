575c33e959b5799c10a2075f9dfacfcace2c7a95
lingxd <lingxd@gmail.com>
2016-05-14 23:32:37 -0700

    actor

---
+++ MyActorApplication/MyActor/MyActor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using Microsoft.ServiceFabric.Actors.Client;
+using MyActor.Interfaces;
+
+namespace MyActor
+{
+    /// <remarks>
+    /// This class represents an actor.
+    /// Every ActorID maps to an instance of this class.
+    /// The StatePersistence attribute determines persistence and replication of actor state:
+    ///  - Persisted: State is written to disk and replicated.
+    ///  - Volatile: State is kept in memory only and replicated.
+    ///  - None: State is kept in memory only and not replicated.
+    /// </remarks>
+    [StatePersistence(StatePersistence.Persisted)]
+    internal class MyActor : Actor, IMyActor
+    {
+        public Task<string> HelloWorld()
+        {
+            return Task.FromResult("Hello World!");
+        }
+    }
+}
